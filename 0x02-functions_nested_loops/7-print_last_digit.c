ada306799576ebe3b6a6e19b0ac3263639ab9853
Peter <petermuringo.com>
2023-03-21 17:40:27 -0700

    8-

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,11 +4,9 @@
  * print_last_digit - prints the last digit of a number..
  * @c: The character to be checked
  * Return:  value of the last digit
- *
  */
-
 int print_last_digit(int n)
-{
+{ 
 int last;
 last = n % 10;
 if (last < 0)
