036aae0f3e8c1d2de9deda74e914aada6cf6ea7b
Peter <petermuringo.com>
2023-03-22 15:02:03 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,12 +8,12 @@
 
 void times_table(void)
 {
-        int a, b, c, d, x;
-
-        for (a = 0; a <= 9; a++)
-        {
-                for (b = 0; b <= 9; b++)
-                {
+	int a, b, c, d, x;
+	
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
 			c = a * b;
 			if(b > 9)
 			{
@@ -24,17 +24,17 @@ void times_table(void)
 				_putchar(x + '0');
 				_putchar(d + '0');
 			}
-else
-{
-if (b != 0)
-{
-_putchar(44);
-_putchar(32);
-_putchar(32);
-}
-_putchar(c + '0');
-}
-}
-_putchar('\n');
-}						
+			else
+			{
+				if (b != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(c + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
