9382a4eba7b669a508f98a418bb2751dc1e2aa5a
Peter <petermuringo.com>
2023-03-22 15:32:47 -0700

    10-main.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,14 +2,13 @@
 
 /**
  * add - add two values
- * @n1
- * @n2
+ * @n1: first no to be computed
+ * @n2: second no to be computed
  * Integer value
  *
  */
 
-int add(int n1, int n2)
-
+int add(int n1,int n2)
 {
 	return (n1 + n2);
 }
