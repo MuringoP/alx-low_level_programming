da8f426d5b76f9cc1f78d296173fce58a140164e
Peter <petermuringo.com>
2023-05-09 09:17:33 -0700

    task 0 Requiescat

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile- Read and print to STDOUT file.
+ * @filename: text file being read
+ * @letters: no of letters to be read
+ * Return: i- no of bytes read and printed, 0 when filename is NULL or function fails
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t a;
+	ssize_t i;
+	ssize_t j;
+
+	a = open(filename, O_RDONLY);
+	if (a == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	j = read(a, buf, letters);
+	i = write(STDOUT_FILENO, buf, j);
+
+	free(buf);
+	close(a);
+	return (i);
+}
